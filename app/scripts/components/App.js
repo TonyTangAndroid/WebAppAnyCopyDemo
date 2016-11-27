4e6dc8fc91d9396e52f36be8827fa983bcb0a1b9
TonyTandAndroid <tonytangandroid@gmail.com>
2016-11-26 19:07:16 -0500

    update devil key

--- app/scripts/components/App.js
+++ app/scripts/components/App.js
@@ -9,7 +9,7 @@ import store from '../store/configureStore'
 //TThis is for dev.
 //Parse.initialize('Qe5rFk8qdUYnTURwyqIuEIRPFXonnFGujWpASGuM', 'WHhs8MnVrfNQLtXPyYQUXLJ6tMPtLg1xOX6ShJLR');
 //TThis is for prod.
-Parse.initialize('yfEdqI7Acp4gilWKcLlpFXm8izVHobgfYLOxkd0s', '0v6U0g9GvPyB3aMucQe9YS2St0Vq7ZwEfWLyBogb');
+Parse.initialize('BXFoDzovArJXaACz34caHCT2MU7s6ob8tizOqrlH', '3BFG9b0MmBiKcVJqyq1omLAzuXvBwRUi718KzXGm');
 
 export default class App extends React.Component {
 	constructor() {
